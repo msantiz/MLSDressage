463ae145ab855381cb11e57f1f3440f1f3577adc
msantiz <60795990+msantiz@users.noreply.github.com>
2020-06-26 16:02:43 -0400

    yes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> 62c50caed168900022e23d697654e431ccb3d5a0
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
@@ -10,10 +6,6 @@ use App\Mail\ContactMail;
 use App\Http\Requests\Contact;
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 62c50caed168900022e23d697654e431ccb3d5a0
 class ContactController extends Controller
 {
     public function store(Contact $request){
@@ -21,8 +13,4 @@ class ContactController extends Controller
         Mail::send(new ContactMail($request));
         return redirect('/');
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 62c50caed168900022e23d697654e431ccb3d5a0
