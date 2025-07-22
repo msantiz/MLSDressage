ba4b8164567ebd0767cf5ae723d2b9a3b7a0be97
msantiz <60795990+msantiz@users.noreply.github.com>
2025-07-22 07:19:34 -0400

    Fix incorrect URL provider import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (env('APP_ENV') === 'production') {
         URL::forceScheme('https');
-        
+
     }
     }
 }
